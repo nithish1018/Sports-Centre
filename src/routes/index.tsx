e1a8c66d75ee77419d6a593f43f892994917c00c
nithish1018 <nithishsiliveru18@gmail.com>
2023-09-02 17:26:46 +0530

    fixed eslint issue

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Navigate, createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Signin from "../pages/sigin";
 import SignupForm from "../pages/signup";
 import Hello from "../pages/index/Hello";

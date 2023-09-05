218e2d4b3c546a45a4289d97dc21448e0d0b4297
nithish1018 <nithishsiliveru18@gmail.com>
2023-09-05 20:27:26 +0530

    fixed redirect issue in sigin

--- src/pages/sigin/SigninForm.tsx
+++ src/pages/sigin/SigninForm.tsx
@@ -37,7 +37,7 @@ const SigninForm: React.FC = () => {
       localStorage.setItem("authToken", data.token);
       localStorage.setItem("userData", JSON.stringify(data.user));
 
-      nav("/matches");
+      nav("/home/matches");
     } catch (error) {
       console.error("Sign-in failed:", error);
     }

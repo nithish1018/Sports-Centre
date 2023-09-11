d21e1da5cb821a82e1bc395c6d552d3ec80991d2
nithish1018 <nithishsiliveru18@gmail.com>
2023-09-11 19:59:02 +0530

    Added PWA

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ThemeProvider } from "./context/theme";
+import { ThemeProvider } from "./context/theme.tsx";
 import { GamesProvider } from "./context/Games/context.tsx";
 import { ArticlesProvider } from "./context/Articles/context.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
